c927f7fe19235e19c6981c8b780411a72a328696
SuhailSherief <suhailscreed@gmail.com>
2023-07-19 23:59:36 +0530

    auth modals

---
+++ src/components/Modal/Auth/Login.tsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import { ModalView, authModalState } from "../../../atoms/authModalAtom";
+import { useSetRecoilState } from "recoil";
+
+type LoginProps = {
+  toggleView: (view: ModalView) => void;
+};
+
+const Login = ({ toggleView }: LoginProps) => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSubmit = () => {};
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Input
+        required
+        name="email"
+        placeholder="email"
+        type="email"
+        mb={2}
+        onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
+      />
+      <Input
+        required
+        name="password"
+        placeholder="password"
+        type="password"
+        mb={2}
+        onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
+      />
+      <Button width="100%" height="36px" mb={2} mt={2} type="submit">
+        Log In
+      </Button>
+      {/* <Flex justifyContent="center" mb={2}>
+        <Text fontSize="9pt" mr={1}>
+          Forgot your password?
+        </Text>
+        <Text
+          fontSize="9pt"
+          color="blue.500"
+          cursor="pointer"
+          onClick={() => toggleView("resetPassword")}>
+          Reset
+        </Text>
+      </Flex> */}
+      <Flex fontSize="9pt" justifyContent="center">
+        <Text mr={1}>New here?</Text>
+        <Text
+          color="blue.500"
+          fontWeight={700}
+          cursor="pointer"
+          onClick={() =>
+            setAuthModalState((prev) => ({
+              ...prev,
+              view: "signup",
+            }))
+          }>
+          SIGN UP
+        </Text>
+      </Flex>
+    </form>
+  );
+};
+export default Login;
