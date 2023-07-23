34267c6674fc3dd227c9764404a10f7a57145743
SuhailSherief <suhailscreed@gmail.com>
2023-07-23 20:14:19 +0530

    updated navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,11 +2,23 @@ import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import SearchInput from "./SearchInput";
 import RightContent from "./RightContent/RightContent";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/src/firebase/clientApp";
+import Directory from "./Directory/Directory";
 
 const Navbar = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justifyContent={{ md: "space-between" }}>
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+        cursor="pointer">
         <Image alt="" src="/images/redditFace.svg" height="30px" />
         <Image
           alt=""
@@ -14,10 +26,10 @@ const Navbar = () => {
           height="46px"
           display={{ base: "none", md: "unset" }}
         />
-        {/* <Directory /> */}
-        <SearchInput />
-        <RightContent />
       </Flex>
+      {user && <Directory />}
+      <SearchInput user={user} />
+      <RightContent user={user} />
     </Flex>
   );
 };
