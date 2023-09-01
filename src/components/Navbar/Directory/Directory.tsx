ddac14f8877fe3fb597380c67a7df977c9bca10c
SuhailSherief <suhailscreed@gmail.com>
2023-09-01 11:28:13 +0530

    navbar update

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -1,38 +1,76 @@
+import React, { useState } from "react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Flex, Icon, Menu, MenuButton, MenuList, Text } from "@chakra-ui/react";
-import { TiHome } from "react-icons/ti";
+import {
+  Box,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import useDirectory from "../../../hooks/useDirectory";
 import Communities from "./Communities";
 
-const UserMenu = () => {
+const Directory = () => {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+
+  const { directoryState, toggleMenuOpen } = useDirectory();
+
   return (
-    <Menu>
-      <MenuButton
-        cursor="pointer"
-        padding="0px 6px"
-        borderRadius={4}
-        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
-        mr={2}
-        ml={{ base: 0, md: 2 }}>
-        <Flex
-          align="center"
-          justify="space-between"
-          width={{ base: "auto", lg: "200px" }}>
-          <Flex align="center">
-            <Icon as={TiHome} fontSize={24} mr={{ base: 1, md: 2 }} />
-            <Flex display={{ base: "none", lg: "flex" }}>
-              <Text fontWeight={600} fontSize="10pt">
-                Home
-              </Text>
+    <Menu isOpen={directoryState.isOpen}>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            cursor="pointer"
+            padding="0px 6px"
+            borderRadius="4px"
+            _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+            mr={2}
+            ml={{ base: 0, md: 2 }}
+            onClick={toggleMenuOpen}>
+            <Flex
+              alignItems="center"
+              justifyContent="space-between"
+              width={{ base: "auto", lg: "200px" }}>
+              <Flex alignItems="center">
+                <>
+                  {directoryState.selectedMenuItem.imageURL ? (
+                    <Image
+                      borderRadius="full"
+                      boxSize="24px"
+                      src={directoryState.selectedMenuItem.imageURL}
+                      mr={2}
+                    />
+                  ) : (
+                    <Icon
+                      fontSize={24}
+                      mr={{ base: 1, md: 2 }}
+                      color={directoryState.selectedMenuItem.iconColor}
+                      as={directoryState.selectedMenuItem.icon}
+                    />
+                  )}
+                  <Box
+                    display={{ base: "none", lg: "flex" }}
+                    flexDirection="column"
+                    fontSize="10pt">
+                    <Text fontWeight={600}>
+                      {directoryState.selectedMenuItem.displayText}
+                    </Text>
+                  </Box>
+                </>
+              </Flex>
+              <ChevronDownIcon color="gray.500" />
             </Flex>
-          </Flex>
-          <ChevronDownIcon />
-        </Flex>
-      </MenuButton>
-      <MenuList>
-        <Communities />
-      </MenuList>
+          </MenuButton>
+          <MenuList maxHeight="300px" overflow="scroll" overflowX="hidden">
+            <Communities menuOpen={isOpen} />
+          </MenuList>
+        </>
+      )}
     </Menu>
   );
 };
-
-export default UserMenu;
+export default Directory;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,9 +5,12 @@ import RightContent from "./RightContent/RightContent";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/src/firebase/clientApp";
 import Directory from "./Directory/Directory";
+import useDirectory from "@/src/hooks/useDirectory";
+import { defaultMenuItem } from "@/src/atoms/directoryMenuAtom";
 
 const Navbar = () => {
   const [user, loading, error] = useAuthState(auth);
+  const { onSelectMenuItem } = useDirectory();
   return (
     <Flex
       bg="white"
@@ -18,7 +21,8 @@ const Navbar = () => {
         align="center"
         width={{ base: "40px", md: "auto" }}
         mr={{ base: 0, md: 2 }}
-        cursor="pointer">
+        cursor="pointer"
+        onClick={() => onSelectMenuItem(defaultMenuItem)}>
         <Image alt="" src="/images/redditFace.svg" height="30px" />
         <Image
           alt=""
